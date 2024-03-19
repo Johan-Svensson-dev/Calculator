f7a7576153ee8210775e6c8c8a20fe70a0cd4832
Johan <johan.svensson@gtsmarketing.se>
2024-03-19 10:31:19 +0100

    css and failed js

--- js/script.js
+++ js/script.js
@@ -25,64 +25,85 @@ function buttonClick(e) {
     // kollar om siffertangent är nedtryckt
     if (btn.substring(0, 1) === 'b') {
         let digit = btn.substring(1, 2); // plockar ut siffran från id:et
-
+        addDigit(digit);
     } else {
-        switch  (btn){
+        switch (btn) {
             case 'add':
+                arithmetic = '+';
+                addDigit('+')
+                break;
             case 'sub':
-            case 'mul': 
-            case 'div':        
-                setOperator(btn);
+                arithmetic = '-';
+                break;
+            case 'mul':
+                arithmetic = '*';
+                break;
+            case 'div':
+                arithmetic = '/';
+                break;
+            case 'clear':
+                clearLCD();
+                break;
+            case 'enter':
+                calculate();
                 break;
             case 'comma':
-                
-                
+                addComma();
+                break;
+        }
+    }
 
-
-        }  // Inte en siffertangent, övriga tangenter.
+    /**
+     *  Lägger till siffra på display.
+     */
+    function addDigit(digit) {
+        lcd.value += digit;
 
     }
-}
 
-/**
- *  Lägger till siffra på display.
- */
-function addDigit(digit) {
-}
+    /**
+     * Lägger till decimaltecken
+     */
+    function addComma() {
+        
+    }
 
-/**
- * Lägger till decimaltecken
- */
-function addComma() {
+    function addPlus(){
+        if (onclick == )
+    }
+    
+    }
 
-}
+    /**
+     * Sparar operator.
+     * +, -, *, /
+     */
+    function setOperator(operator) {
+       
 
-/**
- * Sparar operator.
- * +, -, *, /
- */
-function setOperator(operator){
+        }
+        clearLCD();
+    }
 
-}
+    /**
+     * Beräknar och visar resultatet på displayen.
+     */
+    function calculate() {
 
-/**
- * Beräknar ovh visar resultatet på displayen.
- */
-function calculate() {
 
-}
+    }
 
-/** Rensar display */
-function clearLCD() {
-    lcd.value = '';
-    isComma = false;
-}
+    /** Rensar display */
+    function clearLCD() {
+        lcd.value = '';
+        isComma = false;
+    }
 
-/** Rensar allt, reset */
-function memClear(){
-    memory = 0;
-    arithmetic = null;
-    clearLCD();
-}
+    /** Rensar allt, reset */
+    function memClear() {
+        memory = 0;
+        arithmetic = null;
+        clearLCD();
+    }
 
-window.onload = init;
+    window.onload = init;
